0103b98d44db8fc3e2582775ee53942517fc280f
shahawi <shahawi273@gmail.com>
2015-11-03 00:30:51 +0200

    3.05

---
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -0,0 +1,65 @@
+package com.example.android.sunshine.app;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class SettingsActivity extends PreferenceActivity
+        implements Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add 'general' preferences, defined in the XML file
+        // TODO: Add preferences from XML
+
+        // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
+        // updated when the preference changes.
+        // TODO: Add preferences
+    }
+
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(this);
+
+        onPreferenceChange(preference, PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), "")
+
+        );
+    }
+
+    /**
+     * Called when a Preference has been changed by the user. This is
+     * called before the state of the Preference is about to be updated and
+     * before the state is persisted.
+     *
+     * @param preference The changed Preference.
+     * @param value      The new value of the Preference.
+     * @return True to update the state of the Preference with the new value.
+     */
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        String stringValue = value.toString();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            preference.setSummary(stringValue);
+        }
+
+
+        return true;
+    }
+
+
+}
+
+
+
+
